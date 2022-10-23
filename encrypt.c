0fec4e5af1622a9c2972c81471db876e1f566fce
yvbal <jabaltazar3@up.edu.ph>
2022-10-23 16:20:16 +0800

    Updated encrypt.c to use new method.

--- encrypt.c
+++ encrypt.c
@@ -1,15 +1,34 @@
 #include "methods.h"
 
-// Encrypts text given an unsigned character array.
-unsigned char *encrypt(unsigned char stringArray[]) {
-    transformArray();
+// Encrypts text given an unsigned character array. Appends to result[].
+void encrypt(unsigned char stringArray[], int isCode) {
 
-    // encrypt
-    for (int i=0; i<strlen(stringArray); i++) {
-        temp[i] = transformed[stringArray[i]];
+    // if it is for encrypting code
+    if (isCode) {
+
+        // compute first character for temp[0]
+        int sum = 0;
+        for (int i=0; i<strlen((char*)stringArray); i++) {
+            sum += code[i]*(i+1);
+        }
+        char ascii_cmp = sum % USABLE_CHARS;
+
+        // assign temp[0] and others to temp
+        int i=0;
+        result[i++] = ascii_cmp;
+        transformArray();
+        while (i<CODE_MAXLENGTH) {
+            result[i++] = transformed[USABLE_CHARS - EXTRANEOUS - 1 + (rand()%50)];
+        }
+
+    // if it is for encrypting password or string
+    } else {
+        if (!is_transformed) transformArray();
+        int result_index = strlen((char*)result)-1;
+        for (int i=0; i<strlen((char*)stringArray); i++) {
+            result[result_index+i] = transformed[stringArray[i]];
+        }
     }
-
-    return temp;
 };
 
 int main() {
@@ -22,16 +41,24 @@ int main() {
     scanText(code, 2);
     printf("Using password: ");
     scanText(password, 3);
-
-    //encrypt
-    unsigned char splitter[] = ">";
-    strncat(result, encrypt(code), strlen(code));   cleanTemp();
-    strncat(result, splitter, 1);
-    strncat(result, encrypt(password), strlen(password));   cleanTemp();
-    strncat(result, splitter, 1);
-    strncat(result, encrypt(string), strlen(string));   cleanTemp();
-
     
+    //encrypt
+    encrypt(code, 1);
+
+    unsigned char splitter[2];
+    if (transformed[USABLE_CHARS-1] == 255) splitter[0] = 0;
+        else splitter[0] = transformed[USABLE_CHARS-1];
+    if (transformed[USABLE_CHARS-2] == 255) splitter[1] = 0;
+        else splitter[1] = transformed[USABLE_CHARS-2];
+    
+    result[strlen((char*)result)+1] = splitter[0];
+
+    encrypt(password, 0);
+    
+    result[strlen((char*)result)+1] = splitter[1];
+
+    encrypt(string, 0);
+
     // file print
     fprintf(fp, "%s", result);
     
--- methods.h
+++ methods.h
@@ -5,15 +5,16 @@
 #define STR_MAXLENGTH 1000
 #define CODE_MAXLENGTH 100
 #define PWD_MAXLENGTH 100
-#define ENCRYPTION_VAL 50
 #define USABLE_CHARS 254
+#define EXTRANEOUS 50
 #define FILENAME "output.ecf"
 
-char string[STR_MAXLENGTH];
-char code[CODE_MAXLENGTH];
-char password[PWD_MAXLENGTH];
+unsigned char string[STR_MAXLENGTH];
+unsigned char code[CODE_MAXLENGTH];
+unsigned char password[PWD_MAXLENGTH];
 unsigned char temp[STR_MAXLENGTH];
 unsigned char transformed[USABLE_CHARS];
+int is_transformed = 0;
 
 unsigned char result[STR_MAXLENGTH + PWD_MAXLENGTH + CODE_MAXLENGTH + 2];
 
@@ -25,14 +26,14 @@ void cleanTemp() {
 }
 
 // Allows you to scan properly to give way for other inputs.
-void scanText(char stringArray[], int size_type) {
+void scanText(unsigned char stringArray[], int size_type) {
     
     // determine size
     int size=0;
     if (size_type == 1) size = STR_MAXLENGTH;
         else if (size_type == 2) size = CODE_MAXLENGTH;
         else size = PWD_MAXLENGTH; 
-    
+        
     // get string
     fgets(stringArray, size, stdin);
 }
@@ -42,8 +43,8 @@ void transformArray() {
 
     // compute sum
     int sum = 0;
-    for (int i=0; i<strlen(code); i++) {
-        sum += code[i]*(i+1);        
+    for (int i=0; i<strlen((char*)code); i++) {
+        sum += code[i]*(i+1);
     }
 
     // initialize tranformed array
@@ -53,36 +54,38 @@ void transformArray() {
             else transformed[i] = factor-i;        
     }
 
+    is_transformed = 1;
 }
 
-// Decrypts input file to result
-void decrypt(FILE *fp) {
 
-    // load strings to appropriate arrays
-    int i=0, code_done=0, pwd_done=0;
-    char ascii_cmp=transformed[USABLE_CHARS-1];    
-    while (fp != EOF && i<(STR_MAXLENGTH + PWD_MAXLENGTH + CODE_MAXLENGTH + 2)) {
-        character = fgetc(fp);
+// // Decrypts input file to result
+// void decrypt(FILE *fp) {
+
+//     // 1. load strings to appropriate arrays
+//     int i=0, code_done=0, pwd_done=0;
+//     unsigned char ascii_cmp=0;    
+//     while (fp != EOF && i<(STR_MAXLENGTH + PWD_MAXLENGTH + CODE_MAXLENGTH + 2)) {
+//         character = fgetc(fp);
         
-        // place to code[] array if it is for the code.
-        if (!code_done) {
+//         // place to code[] array if it is for the code.
+//         if (!code_done) {
+//             if (ascii_cmp+1 == 256) ascii_cmp = 0;
+//             if (character == ascii_cmp) code_done = 1;
+//                 else result[i++] = character;
 
-            if (ascii_cmp+1 == 256) ascii_cmp = 0;
-            if (character == ascii_cmp) code_done = 1;
-                else result[i++] = character;
+//         // place to password[] array if it is for the password.
+//         } else if (!pwd_done) {
+//             if (ascii_cmp+2 == 256) ascii_cmp = 0;
+//             if (character == ascii_cmp) pwd_done = 1;
+//                 else result[i++] = character;
 
-        // place to password[] array if it is for the password.
-        } else if (!pwd_done) {
-
-            if (ascii_cmp+2 == 256) ascii_cmp = 0;
-            if (character == ascii_cmp) pwd_done = 1;
-                else result[i++] = character;
-
-        // else, it is a for the string. Place to string[]
-        } else {
-            result[i++] = character
-        };
+//         // else, it is a for the string. Place to string[]
+//         } else {
+//             result[i++] = character
+//         };
         
-    }
+//     }
 
-}
+//     // 2. Decrypt using 
+
+// }
